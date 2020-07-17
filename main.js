c9bfe092e445db5d1d8478e85dfdcc99ddf5cd15
Rade69 <radovan1969@gmail.com>
2020-07-17 23:25:37 +0200

    Nova funkcija

--- main.js
+++ main.js
@@ -18,13 +18,49 @@ function FizzBuzzA(value1,value2) {
   return returnValue;
 }
 
+function FizzBuzzB(value1,value2) {
+  let returnValue = "";
+  let Fizz = false;
+  let Buzz = false;
+  for(let i = 1; i <= 100; i++) {
+    Fizz = i % value1 == 0;
+    Buzz = i % value2 == 0;
+    switch(true) {
+      case Fizz && Buzz :{
+        returnValue += 'FizzBuzz ';
+        break;
+      }
+      case Fizz :{
+        returnValue += 'Fizz';
+        break;
+      }
+      case Buzz :{
+        returnValue += 'Buzz ';
+        break;
+      }
+      default :{ 
+        returnValue += i +' ';
+        break;
+      }
+    }
+  }
+}
+
+return returnValue;
+
+// function FizzBuzzC(value1,value2){
+//   for (var i = 1; i <= 100; i++) {
+//     return (i % 3 == 0 && i % 5 == 0 ? 'FizzBuzz' : (i % 3 == 0 ? 'Fizz' : (i % 5 == 0 && i % 3 != 0 ? 'Buzz' : num)));
+//   }
+// }
+
 //Call and use FizzBuzz
 
 function buzzIt() {
   let output="";
   let val1 = document.getElementById('Fizzvalue').value;
   let val2 = document.getElementById('Buzzvalue').value;
-  output = FizzBuzzA(val1,val2);
+  output = FizzBuzzB(val1,val2);
   document.getElementById('results').innerHTML = output;  
   
 }
